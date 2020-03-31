07b585e9bd6189fdd6cedb029fe972750d5a16c0
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-03-31 17:54:45 +0800

    Update ee.py

--- ee.py
+++ ee.py
@@ -21,7 +21,7 @@ import pylcs
 maxlen = 128
 epochs = 20
 batch_size = 32
-learing_rate = 1e-5
+learning_rate = 1e-5
 crf_lr_multiplier = 1000  # 必要时扩大CRF层的学习率
 
 # bert配置
@@ -116,7 +116,7 @@ model.summary()
 
 model.compile(
     loss=CRF.sparse_loss,
-    optimizer=Adam(learing_rate),
+    optimizer=Adam(learning_rate),
     metrics=[CRF.sparse_accuracy]
 )
 
